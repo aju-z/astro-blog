cbeb7ffeb4afb061b4a2bf045cc3d2d91a68afe4
Aju <ajees2k2@gmail.com>
2023-07-16 19:29:28 +0530

    rss site update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aju-blog-vercel.app",
+  site: "https://astro-blog-aju-z.vercel.app/",
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
